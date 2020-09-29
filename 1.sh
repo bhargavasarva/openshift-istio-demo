0927a43af8bde8d8797daf0bf1894a91ad9bd74f
bhargav <bhargava.sarva@gmail.com>
2020-09-29 22:27:25 +0530

    First commit

---
+++ 1.sh
@@ -0,0 +1,5 @@
+while true
+do curl http://movie-rating-app-movie-rating.134.209.238.216.nip.io/getMovieRating
+echo
+sleep 1
+done
---
+++ apply.sh
@@ -0,0 +1,5 @@
+while true
+do
+curl http://customer-tutorial.143.110.186.157.nip.io
+sleep .1
+done
